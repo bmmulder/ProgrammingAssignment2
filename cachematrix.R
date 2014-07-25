a1355390e0f31736e37ee2f58df1f5d9faa7b2a8
bmmulder <brian@insidestat.nl>
2014-07-25 14:29:04 +0200

    Programming assignment 2
    Added comments
    Added code in function prototypes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "matrix"
+## containing a function to
 
-## Write a short comment describing this function
+## 1 set the value of the matrix
+## 2 get the value of the matrix
+## 3 set the value of the inverted matrix
+## 4 get the value of the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverted special "matrix" 
+## created with the above function. 
+## It first checks to see if there is already an inverted version
+## If so, it gets the inverted matrix from the cache and skips the computation. 
+## Otherwise, it calculates the inverted version sets the value of the inverted matrix
+## in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m    
 }
